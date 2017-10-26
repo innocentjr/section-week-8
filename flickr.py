3fd28971281bfe68302352f7c777be83cc1f67fb
Innocent Obi <icn5@georgetown.edu>
2017-10-26 16:40:44 -0400

    first try

--- flickr.py
+++ flickr.py
@@ -25,19 +25,22 @@ def params_unique_combination(baseurl, params_d, private_keys=["api_key"]):
             res.append("{}-{}".format(k, params_d[k]))
     return baseurl + "_".join(res)
 
-def search_flickr_by_tags(tags):
+def search_flickr(key, value):
     if not FLICKR_API_KEY:
         raise Exception('Flickr API Key is missing!')
 
     baseurl = "https://api.flickr.com/services/rest/"
-    params_diction = {
-        "method": "flickr.photos.search",
-        "format": "json",
-        "api_key": FLICKR_API_KEY,
-        "tags": tags,
-        "per_page": 10,
-        "nojsoncallback": 1
-    }
+        params_diction = {
+            "method": "flickr.photos.search",
+            "format": "json",
+            "api_key": FLICKR_API_KEY,
+            "tags": tags,
+            "per_page": 10,
+            "nojsoncallback": 1
+        }
+    diction = {key:value}
+    params_diction.update(diction)
+
 
     unique_ident = params_unique_combination(baseurl,params_diction)
     if unique_ident in CACHE_DICTION:
@@ -65,7 +68,7 @@ CACHE_DICTION = load_cache_json()
 if DEBUG:
     print(CACHE_DICTION)
 
-results = search_flickr_by_tags('sunset summer')
+results = search_flickr('sunset summer')
 
 photos_list = []
 for r in results['photos']['photo']:
